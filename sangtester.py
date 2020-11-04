f66f8bed7f816c26354c4af0adf71cbfff2ded8c
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2020-11-04 10:07:31 +0100

    Add files via upload

--- sangtester.py
+++ sangtester.py
@@ -3,7 +3,7 @@ from spilleliste import Spilleliste
 import os
 
 allMusikk = Spilleliste('Hele musikkbiblioteket')
-albumer = [Spilleliste('Hele musikkbiblioteket')]
+albumer = [allMusikk]
 #sang1 = Sang("Lady Gaga and Bradley Cooper", "Shallow", "ode_to_joy.wav")
 
 directory = os.getcwd()
@@ -13,8 +13,14 @@ def finnMappeOgLegTilSanger(directory):
     for filename in os.listdir(directory):
         if filename.endswith(".wav"):
             allMusikk.leggTilSang(Sang("", filename[:-4], directory + "\\" + filename))
-            albumer[-1].leggTilSang(Sang("", filename[:-4], directory + "\\" + filename))
-            print("Added: ", os.path.join(directory, filename))
+            # denne for løkken fikser problemet hvis mappen er mellom to .wav filer
+            # som fører til at den siste .wav filen regnes som om den er i mappen
+            for x in albumer:
+                if x.returnNavn() == directory.split("\\")[-1]:
+                    x.leggTilSang(Sang("", filename[:-4], directory + "\\" + filename))
+                    print("Added: ", os.path.join(directory, filename))
+            # istedenfor hele for løkken så var det:
+            # albumer[-1].leggTilSang(Sang("", filename[:-4], directory + "\\" + filename))
         elif filename[-4] != ".":
             albumName = filename
             nyAlbum = Spilleliste(albumName) # legge til albumer
@@ -26,6 +32,7 @@ def finnMappeOgLegTilSanger(directory):
 # stack overflow code so i can loop true the directory and find .wav files
 for filename in os.listdir(directory):
     if filename == "music": # if it's the music folder loop inside it
+        albumer.append(Spilleliste(filename))
         directoryMusic = directory + '\\' + filename
         finnMappeOgLegTilSanger(directoryMusic)
         ## Old code made better using finnMappeOgLegTilSanger() function
@@ -42,26 +49,63 @@ for filename in os.listdir(directory):
         #                 allMusikk.leggTilSang(Sang("", song[:-4], directoryMusic + "\\" + albumName + "\\" + song)) # song[:-4] + song[-4:]
         #                 print("Added: ", os.path.join(directoryMusic, song))
 
+# ser sanger og lar brukeren velge en sang den vil spille i en spilleliste
+def seSanger(spilleliste):
+    print("Velg hvilken sang du vil spille")
+    i = 0
+    for x in spilleliste.returnSanger():
+        print(f"{i + 1}. {spilleliste.returnSangI(i)}")
+        i += 1
+
+    # checks if the playlist/album has any songs in it
+    if i == 0:
+        print("ingen sanger i spillelisten/albumet :c")
+    else:
+        choice = input("velg sang: ")
+        i = 1
+        for x in spilleliste.returnSanger():
+            if choice == str(i):
+                spilleliste.returnSangI(i - 1).spill() # indexen starter med 0 derfor i - 1
+                nowPlaying = spilleliste.returnSangI(i - 1)
+            i += 1
+        
+
+        input("Klikk enter for å avslutte Sangen: ")
+        nowPlaying.stop()
+
+# viser alle albumene i 'albumer' variabelen, og lar brukeren 
+def seAlbumer():
+    i = 0
+    for x in albumer:
+        print(f"{i + 1}. {x.returnNavn()}")
+        i += 1
+    
+    choice = input("velg sang: ")
+    i = 1
+    for x in albumer:
+        if choice == str(i):
+            seSanger(x) # ser sanger og lar brukeren velge en sang den vil spille i en spilleliste
+        i += 1
 
 def hovedprogram():
 
-    print(allMusikk)
-    print("ALBUMER: ",albumer)
+    #print(allMusikk) ## debug
+    #print("ALBUMER: ",albumer) ## debug
     # Metoden spill
-    print("Velg hvilken sang du vil spille")
-    i = 0
-    for x in allMusikk.returnSanger():
-        print(f"{i + 1}. {allMusikk.returnSangI(i)}")
-        i += 1
-    choice = input("velg sang: ")
-    i = 1
-    for x in allMusikk.returnSanger():
-        if choice == str(i):
-            allMusikk.returnSangI(i - 1).spill() # indexen starter med 0 derfor i - 1
-        i += 1
-    input("Klikk enter for å avslutte: ")
+    print("\n\n\n")
+    print("Velg om du vil spille en bestemt album eller se alle sanger, skriv 1 eller 2")
+    print("1. Se alle sanger")
+    print("2. Se alle albumer")
+    print("3. Avlsutt")
 
-    #hovedprogram()
-
+    choice = input("")
+    if choice == "1":
+        print("\n\n\n")
+        seSanger(allMusikk) # ser sanger og lar brukeren velge en sang den vil spille i en spilleliste 
+    elif choice == "2":
+        print("\n\n\n")
+        seAlbumer()
+    if choice != "3":
+        hovedprogram()
 
 hovedprogram()
